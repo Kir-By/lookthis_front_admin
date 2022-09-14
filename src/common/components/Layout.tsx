d65188dc8b109520df0cbd35200f9684f78d07a3
윤동호 <dhboys@vaiv.kr>
2022-09-14 20:23:48 +0900

    :tada: first commit

---
+++ src/common/components/Layout.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: #228be6;
+  color: #ffffff;
+  border: none;
+  border-radius: 5px;
+  padding: 12px 24px;
+  margin: 0;
+`;
+
+const Layout = () => {
+  return <StyledButton>button</StyledButton>;
+};
+
+export default Layout;
